f285680085d07d0f4979a915299ab828e387be22
DJ5harma <sunubhargav2004@gmail.com>
2025-11-27 18:52:29 +0530

    another way

--- 20_goroutines/goroutines.go
+++ 20_goroutines/goroutines.go
@@ -14,9 +14,20 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	for i := range 10 {
+	for i := range 5 {
 		wg.Add(1)
 		go task(i+1, &wg)
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("Inline task", i)
+		}()
+
+		wg.Go(func() {
+
+			fmt.Println("Direct WG GO task", i)
+		})
 	}
 	wg.Wait()
 

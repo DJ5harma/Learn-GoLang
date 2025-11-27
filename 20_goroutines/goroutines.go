fafb97a77784fd10e222f724db40fb00cbe39459
DJ5harma <sunubhargav2004@gmail.com>
2025-11-27 18:49:23 +0530

    gorouteine

--- 20_goroutines/goroutines.go
+++ 20_goroutines/goroutines.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func task(id int) {
+func task(id int, wg *sync.WaitGroup) {
+	defer wg.Done() //like useEffect's return
 	fmt.Println("Doing task", id)
 }
 
 func main() {
+
+	var wg sync.WaitGroup
+
 	for i := range 10 {
-		go task(i + 1)
-
-		go func(i int) {
-			fmt.Println("item", i)
-		}(i)
+		wg.Add(1)
+		go task(i+1, &wg)
 	}
+	wg.Wait()
 
-	time.Sleep(time.Second * 2)
 }
